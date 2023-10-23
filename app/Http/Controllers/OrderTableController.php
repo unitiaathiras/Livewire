a12469a12ab50732e609a4c89e006138dd870fbb
unitiaathira <athirashibuathi13@gmail.com>
2023-10-23 10:11:19 +0530

    first and completed commit

---
+++ app/Http/Controllers/OrderTableController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class OrderTableController extends Controller
+{
+   public function home(){
+    return 'hi';
+   }
+        
+    }
